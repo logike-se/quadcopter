414d865bc8e26a693774cab507918229f5cfe398
logike-se <johan.westlund@logike.se>
2015-11-26 09:32:35 +0100

    Small fixes

--- sw/android/src/com/quadcopter/logike/se/SensorThread.java
+++ sw/android/src/com/quadcopter/logike/se/SensorThread.java
@@ -20,16 +20,16 @@ public class SensorThread extends HandlerThread implements Handler.Callback, Sen
 	SensorManager sensorMgr;
 	Sensor acc;
 	SensorValues.Accelerometer acc_values;
-		
+
 	public SensorThread(Context context, Handler.Callback callback) {
 		super("Sensor Thread");
 		this.parentHandler = new Handler(Looper.getMainLooper(), callback);
-		
+
 		acc_values = new SensorValues.Accelerometer();
-		
+
 		sensorMgr = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
-        acc = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorMgr.registerListener(this, acc, SensorManager.SENSOR_DELAY_FASTEST);
+		acc = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorMgr.registerListener(this, acc, SensorManager.SENSOR_DELAY_FASTEST);
 	}
 
 	@Override
@@ -45,7 +45,7 @@ public class SensorThread extends HandlerThread implements Handler.Callback, Sen
 		}
 		return true;
 	}
-	
+
 	@Override
 	public boolean quitSafely(){
 		super.quitSafely();
@@ -66,9 +66,9 @@ public class SensorThread extends HandlerThread implements Handler.Callback, Sen
 		msg.arg1 = event.sensor.getType();
 		if(msg.arg1 == Sensor.TYPE_ACCELEROMETER){
 			acc_values.nSamples++;
-			
-			/* Calibration */
+
 			if(acc_values.calibrationSteps > 0){
+				/* Calibration */
 				acc_values.bias.x += event.values[0];
 				acc_values.bias.y += event.values[1];
 				acc_values.bias.z += event.values[2] - Defines.Constants.G;
@@ -79,31 +79,32 @@ public class SensorThread extends HandlerThread implements Handler.Callback, Sen
 					acc_values.bias.z /= acc_values.nSamples;
 				}
 			}
-			
-			/* Save accelerator values */
-			acc_values.acceleration.x = event.values[0] - acc_values.bias.x;
-			acc_values.acceleration.y = event.values[1] - acc_values.bias.y;
-			acc_values.acceleration.z = event.values[2] - acc_values.bias.z;
-			acc_values.angle = getAnglesFromAcceleration(acc_values.acceleration);
-			acc_values.dt = event.timestamp - acc_values.timestamp;
-			acc_values.timestamp = event.timestamp;
-			msg.obj = acc_values;
+			else{
+				/* Save accelerator values */
+				acc_values.acceleration.x = event.values[0] - acc_values.bias.x;
+				acc_values.acceleration.y = event.values[1] - acc_values.bias.y;
+				acc_values.acceleration.z = event.values[2] - acc_values.bias.z;
+				acc_values.angle = getAnglesFromAcceleration(acc_values.acceleration);
+				acc_values.dt = event.timestamp - acc_values.timestamp;
+				acc_values.timestamp = event.timestamp;
+				msg.obj = acc_values;
+			}
 		}
 		else if(msg.arg1 == Sensor.TYPE_GYROSCOPE_UNCALIBRATED){
-			
+
 		}
-		
+
 		parentHandler.sendMessage(msg);
 	}
-	
+
 	/* This function returns pitch and roll angles [-pi/2, pi/2] */
 	private Coordinates getAnglesFromAcceleration(Coordinates a){
 		Coordinates ret = new Coordinates();
-		float xx = a.x * a.x;
-		float yy = a.y * a.y;
-		float zz = a.z * a.z;
-		ret.x = (float) Math.atan(a.x / (yy + zz));
-		ret.y = (float) Math.atan(a.y / (xx + zz));
+		double xx = a.x * a.x;
+		double yy = a.y * a.y;
+		double zz = a.z * a.z;
+		ret.x = (float) Math.atan(a.x / Math.sqrt(yy + zz));
+		ret.y = (float) Math.atan(a.y / Math.sqrt(xx + zz));
 		ret.z = 0f;
 		return ret;
 	}
